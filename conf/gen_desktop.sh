ba3700f235a2f3b116edd6c474242334a8ac652e
Buged <75508556+buged@users.noreply.github.com>
2022-03-01 18:13:06 -0300

    Removed print and sound

--- conf/gen_desktop.sh
+++ conf/gen_desktop.sh
@@ -27,19 +27,6 @@ STR_DESKTOP="\
   # Enable hardware acceleration.
   hardware.opengl.enable = true;
   
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [ pkgs.epson-escpr ];
-  services.avahi.enable = true; # for CUPS to automatically find the printer via Wi-Fi
-  services.avahi.nssmdns = true; # for CUPS to automatically find the printer via Wi-Fi
-  
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  
-  # Update Intel CPU Microcode
-  hardware.cpu.intel.updateMicrocode = true;
-
   # Configure the services required for a desktop.
   services = {
     xserver = {
